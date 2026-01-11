2de16e87c85b1ec56baf920404575eb2aae5eb97
Yuriy <germanovich4job@gmail.com>
2026-01-11 03:32:57 +0300

    prisma 7 init

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
 
-export const prisma = globalThis.prisma || new PrismaClient();
+// export const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalThis.prisma = prisma;
+// }
